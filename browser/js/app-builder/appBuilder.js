964ab6183188b70ae9180d54f220b1d026e60eb9
John Gruska <johngruska@Johns-MacBook-Pro-3.local>
2016-05-21 12:48:56 -0400

    Pushing up changes

--- browser/js/app-builder/appBuilder.js
+++ browser/js/app-builder/appBuilder.js
@@ -9,11 +9,32 @@ app.config(function ($stateProvider) {
         },
         controller: 'AppBuilderCtrl'
     });
+    $stateProvider.state('appBuilder.schema', {
+        url: '/app-builder/:id',
+        templateUrl: 'js/app-builder/schema.html',
+        resolve: {
+            schema: function(SchemaFactory, $stateParams) {
+                return SchemaFactory.fetchByAppId($stateParams.id);
+            }
+        },
+        controller: 'SchemaCtrl'
+    });
 });
 
-app.controller('AppBuilderCtrl', function(apps, $scope) {
+app.controller('AppBuilderCtrl', function(apps, AppFactory, SchemaFactory, $scope) {
 	apps.unshift({id: -1, name: 'Select an App'});
     $scope.appIndex = 0;
     $scope.apps = apps;
-    console.log("Apps", apps);
+    $scope.activeSchema = -1;
+    
+    $scope.loadApp = function() {
+        SchemaFactory.fetchByAppId($scope.apps[$scope.appIndex])
+        .then(function(schemas) {
+            $scope.schemas = schemas;
+        })
+    };
+});
+
+app.controller('SchemaCtrl', function(schema, AppFactory, SchemaFactory, $scope) {
+
 });
