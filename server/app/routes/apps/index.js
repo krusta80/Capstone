fac80d5083c67ad593cee7f265acb650cd9d9851
John Gruska <johngruska@Johns-MacBook-Pro-3.local>
2016-05-20 23:05:57 -0400

    Added express routes for apps, schemas, and fields

---
+++ server/app/routes/apps/index.js
@@ -0,0 +1,54 @@
+'use strict';
+var router = require('express').Router();
+var App = require('../../../db/models/app');
+module.exports = router;
+
+var ensureAuthenticated = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        res.status(401).end();
+    }
+};
+
+router.get('/', ensureAuthenticated, function (req, res) {
+    App.find({User: req.user})
+    .then(function(apps) {
+        res.send(apps);
+    });
+});
+
+router.get('/:appId', ensureAuthenticated, function (req, res) {
+    App.findById(req.params.appId)
+    .then(function(app) {
+        res.send(app);
+    });
+});
+
+router.post('/', ensureAuthenticated, function (req, res) {
+    App.create(req.body)
+    .then(function(app) {
+        res.send(app);
+    });
+});
+
+router.put('/:appId', ensureAuthenticated, function (req, res) {
+    App.findById(req.params.appId)
+    .then(function(app) {
+        Object.keys(app).forEach(function(field) {
+            if(req.body[field])
+                app[field] = req.body[field];
+        })
+        return app.save();
+    })
+    .then(function(app) {
+        req.send(app);
+    });
+});
+
+router.delete('/:appId', ensureAuthenticated, function (req, res) {
+    App.findByIdAndRemove(req.params.appId)
+    .then(function(app) {
+        res.send(app);
+    });
+});
---
+++ server/app/routes/fields/index.js
@@ -0,0 +1,54 @@
+'use strict';
+var router = require('express').Router();
+var Field = require('../../../db/models/field');
+module.exports = router;
+
+var ensureAuthenticated = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        res.status(401).end();
+    }
+};
+
+router.get('/:schemaId', ensureAuthenticated, function (req, res) {
+    Field.find({Schema: req.params.schemaId})
+    .then(function(fields) {
+        res.send(fields);
+    });
+});
+
+router.get('/:fieldId', ensureAuthenticated, function (req, res) {
+    Field.findById(req.params.fieldId)
+    .then(function(field) {
+        res.send(field);
+    });
+});
+
+router.post('/', ensureAuthenticated, function (req, res) {
+    Field.create(req.body)
+    .then(function(field) {
+        res.send(field);
+    });
+});
+
+router.put('/:fieldId', ensureAuthenticated, function (req, res) {
+    Field.findById(req.params.fieldId)
+    .then(function(field) {
+        Object.keys(field).forEach(function(property) {
+            if(req.body[property])
+                field[property] = req.body[property];
+        })
+        return field.save();
+    })
+    .then(function(field) {
+        req.send(field);
+    });
+});
+
+router.delete('/:fieldId', ensureAuthenticated, function (req, res) {
+    Field.findByIdAndRemove(req.params.fieldId)
+    .then(function(field) {
+        res.send(field);
+    });
+});
