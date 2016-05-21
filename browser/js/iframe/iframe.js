fbe3758f4f4d1996ff57ce92e5c22e06005b2b52
antkim003@gmail.com <antkim003@gmail.com>
2016-05-21 15:07:05 -0400

    taking out debugger

--- browser/js/iframe/iframe.js
+++ browser/js/iframe/iframe.js
@@ -12,7 +12,6 @@ app.controller('IframeCtrl', function ($scope, $http) {
     $scope.searchthis = function(url) {
         $http.post('/api/scrape/site', {url: url})
             .then(function(response) {
-                debugger;
                 document.getElementById('iframedisplay').src = response.data.publicDirectory;        
             })
             .catch(function(err) {
