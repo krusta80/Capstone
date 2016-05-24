e9b96528bc3ca9d4d45888a71b62b896a014402e
antkim003@gmail.com <antkim003@gmail.com>
2016-05-23 22:58:32 -0400

    ability to navigate through pages

--- browser/js/iframe/iframe.js
+++ browser/js/iframe/iframe.js
@@ -11,31 +11,16 @@ app.config(function ($stateProvider) {
 app.controller('IframeCtrl', function ($scope, $http) {
     $scope.loaded = false;
     $scope.searchthis = function(url) {
-        // document.getElementById('iframedisplay').src = "/api/scrape/proxy?proxyurl=" + url;
-
-        // this is the downloaded version...
-
-        $http.post('/api/scrape/proxy', {proxyurl: url})
-            .then(function(response) {
-                var iframe = document.getElementById('iframedisplay');
-                iframe.contentWindow.document.open();
-                iframe.contentWindow.document.write(response.data);
-                iframe.contentWindow.document.close();
-
+        document.getElementById('iframedisplay').src = "/api/scrape/proxy?proxyurl=" + url;
                 setTimeout(function() {
                     $scope.loaded = true; 
                     $scope.$apply(); 
                     var iframecontents = $('#iframedisplay').contents()[0];
-                    $(iframecontents).find('body').on('click', function(ev) {
-                        ev.stopPropagation();
-                        ev.preventDefault();
-                    });   
+                    // $(iframecontents).find('body').on('click', function(ev) {
+                    //     ev.stopPropagation();
+                    //     ev.preventDefault();
+                    // });   
                 }, 0)
-
-            })
-            .catch(function(err) {
-                console.log('there was an error');
-            });
 
     };
 });
