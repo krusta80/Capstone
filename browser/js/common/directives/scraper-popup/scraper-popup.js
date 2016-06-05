8af42f901c7b1c8d8a94444b8ad661e19c515cd6
Christopher Kennedy <cjkb90@users.noreply.github.com>
2016-06-05 17:46:47 -0400

    trying to fix

---
+++ browser/js/common/directives/scraper-popup/scraper-popup.js
@@ -0,0 +1,59 @@
+app.directive('scraperPopup', function($rootScope, ScraperPopupFactory){
+  return {
+    restric: 'E',
+    templateUrl: '/js/common/directives/scraper-popup/scraper-popup.html',
+    scope: {},
+    transclude: true,
+    link: function(scope) {
+      scope.popupactivated = false;
+      scope.addRow = function(obj) {
+        ScraperPopupFactory.addRow();
+      };
+
+      scope.getContent = function(arrayOfObj) {
+        return getContent(arrayOfObj);
+      };
+
+      scope.saveData = function(attributes) {
+        var cachedData = ScraperPopupFactory.get();
+        ScraperPopupFactory.save(attributes, cachedData)
+          .then(function(resp) {
+            if (resp.status === 200) {
+              scope.popupactivated = false;  
+            }
+            
+          })
+      };
+
+      $rootScope.$on('click', function(evt, data, coordinates){
+        console.log("data on click:", data);
+        scope.popupactivated = true;
+        scope.left = coordinates.x;
+        scope.top = coordinates.y;
+        ScraperPopupFactory.reset();
+
+        scope.popupData = ScraperPopupFactory.add(data).data;
+        scope.currentContent = ScraperPopupFactory.getContent(scope.popupData);
+        scope.attributes = scope.popupData;
+
+        scope.selection = [];
+        scope.selectedAttributes = function selectedAttribuets() {
+          var output = []
+          scope.attributes.forEach(function(attribute) {
+            if (attribute.selected) {
+              output.push(attribute);
+            }
+          });
+          return output;
+        };
+        scope.$apply();
+      });
+
+    }
+  };
+});
+
+
+
+
+
---
+++ tests/server/utils/scraper.actions.real.spec.js
@@ -0,0 +1,54 @@
+require('../../../server/db/models');
+var chai = require('chai').expect,
+  mongoose = require('mongoose'),
+  Page = mongoose.model('Page'),
+  Job = mongoose.model('Job'),
+  Hist = mongoose.model('ScraperElementHist'),
+  ScraperElem = mongoose.model('ScraperElement'),
+  Promise = require('bluebird');
+
+describe('run job with pre-actions', function(){
+  this.timeout(60000);
+  var job;
+  before(function(done){
+    mongoose.connect('mongodb://localhost/capstone')
+    .then(function(){
+      return Promise.join(Page.remove(), Job.remove(), Hist.remove() );
+    })
+    .then(function(){
+      return ScraperElem.find();
+    })
+    .then(function(scraperElements){
+      return Page.create({
+        title: 'test page',
+        url: 'http://www.msnbc.com',
+        targetElements: scraperElements
+      });
+    })
+    .then(function(page){
+      console.log(page);
+      return Job.create({
+        title: 'test job',
+        pages: [page._id]
+      });
+    })
+    .then(function(_job){
+      //remember to populate pages!
+      return Job.findById(_job._id).populate('pages');
+    })
+    .then(function(_job){
+      job = _job;
+      done();
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+  });
+  it('runs a job with pages with actions', function(done){
+    job.runJob()
+    .then(function(results){
+      console.log(results);
+      done();
+    });
+  });
+});
