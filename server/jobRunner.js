3e5c1224069a44d727afc51c6539c0795464dfb0
John Gruska <johngruska@Johns-MacBook-Pro-3.local>
2016-06-12 14:50:16 -0300

    Basic jobrunner created

--- server/jobRunner.js
+++ server/jobRunner.js
@@ -1,5 +1,6 @@
 'use strict';
 var mongoose = require('mongoose');
+var models = require('./db')
 var Job = mongoose.model('Job');
 var Project = mongoose.model('Project');
 
@@ -9,15 +10,21 @@ var jobList = [];
 Project.find({}).populate('jobs.pages')
 .then(function(projects) {
 	projects.forEach(function(project) {
+		console.log("Project is", project);
 		project.jobs.forEach(function(job) {
 			if(job.active)
 				jobList.push(job.runJob(project));
 		});
 	});
+	console.log("Job list is", jobList);
 	return Promise.all(jobList)
 })
 .then(function(resultList) {
 	resultList.forEach(function(result) {
-		console.log(result);
+		console.log("result is",result);
 	})
+	process.exit();
+})
+.catch(function(err) {
+	console.log("Error:", err);
 })
