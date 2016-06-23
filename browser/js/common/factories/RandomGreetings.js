64e2c577c613fe5b63917c399c2a2ce9a4416d69
distres117 <omcrobbie@gmail.com>
2016-06-23 14:39:12 -0400

    done

--- browser/js/common/factories/RandomGreetings.js
+++ browser/js/common/factories/RandomGreetings.js
@@ -9,7 +9,7 @@ app.factory('RandomGreetings', function () {
         'At long last, I live!',
         'Hello, simple human.',
         'What a beautiful day!',
-        'I\'m like any other project, except that I am yours. :)',
+        'Mmmm..Data',
         'This empty string is for Lindsay Levine.',
         'こんにちは、ユーザー様。',
         'Welcome. To. WEBSITE.',
